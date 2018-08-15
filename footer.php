0a4c10e6697f2fd15fe7ad55dd96dcb5ca144ad4
David Duffy <daviduffy1@gmail.com>
2018-08-15 13:31:02 -0700

    added social media urls

--- footer.php
+++ footer.php
@@ -1,22 +1,17 @@
 <footer class="footer u-flex u-flex--c">
   <ul class="u-flex">
     <li>
-        <a href="https://www.facebook.com/">
+        <a href="https://www.facebook.com/helenafalangus">
             <i class="fa fa-facebook" aria-hidden="true"></i>
         </a>
     </li>
     <li>
-        <a href="https://twitter.com/">
-            <i class="fa fa-twitter" aria-hidden="true"></i>
-        </a>
-    </li>
-    <li>
-        <a href="https://www.instagram.com/">
+        <a href="https://www.instagram.com/helena_knows_design">
             <i class="fa fa-instagram" aria-hidden="true"></i>
         </a>
     </li>
     <li>
-        <a href="https://www.pinterest.com/">
+        <a href="https://www.pinterest.com/helena43/">
             <i class="fa fa-pinterest" aria-hidden="true"></i>
         </a>
     </li>
